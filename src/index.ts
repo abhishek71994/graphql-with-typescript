dd0c8a48ef4aef64e4a0922b2678abc83d1d6984
abhishek71994 <abhishek71994@gmail.com>
2021-02-08 16:12:14 +0800

    Express App setup

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,23 @@
-console.log("hoorah! Cant say anything else.");
+import { execPath } from "process";
+
+const express = require('express');
+const morgan = require('morgan');
+
+
+const app = express();
+app.use(morgan('tiny'));
+app.use(function (req: any, res: any, next: any) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET, PUT, POST");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
+
+app.get("/", (req: any, res: any) => {
+  res.send({ "foo": false });
+})
+
+app.listen(3000, () => {
+  console.log(`App running on port`);
+})
